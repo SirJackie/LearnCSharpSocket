bdbf8c6c31e0f801b197cde7d6a00c62c92b3f47
lyj00912 <3122742760@qq.com>
2022-02-05 22:47:07 +0800

    refactored the file structure

---
+++ RecvLengthAndMessage/Client/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace Client
+{
+    class Client
+    {
+        public static void Main(string[] arg)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 1302);  // is equal to s.connect()
+                NetworkStream stream = client.GetStream();
+
+                //
+                // Write a Message
+                //
+
+                string msg = "My name is Neo";
+                byte[] msgBin = Encoding.UTF8.GetBytes(msg);
+                stream.Write(msgBin, 0, msgBin.Length);
+
+                //
+                // Read a Message
+                //
+
+                byte[] buffer = new byte[1024];
+                stream.Read(buffer, 0, buffer.Length);
+
+                // Count the Length of the Byte Array received
+                int recv = 0;
+                foreach (byte b in buffer)
+                {
+                    if (b != 0)
+                    {
+                        recv++;
+                    }
+                }
+
+                string request = Encoding.UTF8.GetString(buffer, 0, recv);
+                Console.WriteLine(request);
+
+                stream.Close();
+                client.Close();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Failed to connect...");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RecvLengthAndMessage/Server/Server.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Server
+    {
+        public static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1302);  // is equal to s.bind()
+            listener.Start();
+            while(true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client accepted.");
+                NetworkStream stream = client.GetStream();
+                try
+                {
+                    //
+                    // Read a Message
+                    //
+
+                    byte[] buffer = new byte[1024];
+                    stream.Read(buffer, 0, buffer.Length);
+
+                    // Count the Length of the Byte Array received
+                    int recv = 0;
+                    foreach (byte b in buffer)
+                    {
+                        if(b != 0)
+                        {
+                            recv++;
+                        }
+                    }
+
+                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
+                    Console.WriteLine(request);
+
+
+                    //
+                    // Write a Message
+                    //
+
+                    string msg = "OK, I know your name is Leo.";
+                    byte[] msgBin = Encoding.UTF8.GetBytes(msg);
+                    stream.Write(msgBin, 0, msgBin.Length);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong.");
+                }
+            }
+        }
+    }
+}
