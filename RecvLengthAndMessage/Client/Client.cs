52ac48c128a7c5d6f8d4ec9c2984951e67dd4cc5
lyj00912 <3122742760@qq.com>
2022-02-05 23:54:59 +0800

    simplified the Read and Write Message Operation

--- RecvLengthAndMessage/Client/Client.cs
+++ RecvLengthAndMessage/Client/Client.cs
@@ -7,6 +7,30 @@ namespace Client
 {
     class Client
     {
+        public static void SendMessage(NetworkStream stream, string msg)
+        {
+            byte[] msgBin = Encoding.UTF8.GetBytes(msg);
+            stream.Write(msgBin, 0, msgBin.Length);
+        }
+
+        public static string RecvMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[1024];
+            stream.Read(buffer, 0, buffer.Length);
+
+            // Count the Length of the Byte Array received
+            int recv = 0;
+            foreach (byte b in buffer)
+            {
+                if (b != 0)
+                {
+                    recv++;
+                }
+            }
+
+            return Encoding.UTF8.GetString(buffer, 0, recv);
+        }
+
         public static void Main(string[] arg)
         {
             try
@@ -14,34 +38,9 @@ namespace Client
                 TcpClient client = new TcpClient("127.0.0.1", 1302);  // is equal to s.connect()
                 NetworkStream stream = client.GetStream();
 
-                //
-                // Write a Message
-                //
-
-                string msg = "My name is Neo";
-                byte[] msgBin = Encoding.UTF8.GetBytes(msg);
-                stream.Write(msgBin, 0, msgBin.Length);
-
-                //
-                // Read a Message
-                //
-
-                byte[] buffer = new byte[1024];
-                stream.Read(buffer, 0, buffer.Length);
-
-                // Count the Length of the Byte Array received
-                int recv = 0;
-                foreach (byte b in buffer)
-                {
-                    if (b != 0)
-                    {
-                        recv++;
-                    }
-                }
-
-                string request = Encoding.UTF8.GetString(buffer, 0, recv);
-                Console.WriteLine(request);
-
+                SendMessage(stream, "My name is Sir Jackie.");
+                Console.WriteLine(RecvMessage(stream));
+                
                 stream.Close();
                 client.Close();
 
--- RecvLengthAndMessage/Server/Server.cs
+++ RecvLengthAndMessage/Server/Server.cs
@@ -7,6 +7,30 @@ namespace Server
 {
     class Server
     {
+        public static void SendMessage(NetworkStream stream, string msg)
+        {
+            byte[] msgBin = Encoding.UTF8.GetBytes(msg);
+            stream.Write(msgBin, 0, msgBin.Length);
+        }
+
+        public static string RecvMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[1024];
+            stream.Read(buffer, 0, buffer.Length);
+
+            // Count the Length of the Byte Array received
+            int recv = 0;
+            foreach (byte b in buffer)
+            {
+                if (b != 0)
+                {
+                    recv++;
+                }
+            }
+
+            return Encoding.UTF8.GetString(buffer, 0, recv);
+        }
+
         public static void Main(string[] args)
         {
             TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1302);  // is equal to s.bind()
@@ -18,34 +42,8 @@ namespace Server
                 NetworkStream stream = client.GetStream();
                 try
                 {
-                    //
-                    // Read a Message
-                    //
-
-                    byte[] buffer = new byte[1024];
-                    stream.Read(buffer, 0, buffer.Length);
-
-                    // Count the Length of the Byte Array received
-                    int recv = 0;
-                    foreach (byte b in buffer)
-                    {
-                        if(b != 0)
-                        {
-                            recv++;
-                        }
-                    }
-
-                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
-                    Console.WriteLine(request);
-
-
-                    //
-                    // Write a Message
-                    //
-
-                    string msg = "OK, I know your name is Leo.";
-                    byte[] msgBin = Encoding.UTF8.GetBytes(msg);
-                    stream.Write(msgBin, 0, msgBin.Length);
+                    Console.WriteLine(RecvMessage(stream));
+                    SendMessage(stream, "OK, I know your name is Leo.");
 
                 }
                 catch (Exception e)
